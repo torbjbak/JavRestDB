562addc6bb116b90d851785a3f42b509edc654f1
torbjbak <tbakke93@gmail.com>
2021-02-17 14:18:56 +0100

    ugly getAuthorsBook and getBooksAuthor methods

--- src/main/java/fullstack/oving2/web/ABController.java
+++ src/main/java/fullstack/oving2/web/ABController.java
@@ -1,12 +1,14 @@
 package fullstack.oving2.web;
 
-import fullstack.oving2.model.Author;
 import fullstack.oving2.model.AuthorBook;
 import fullstack.oving2.service.MyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
@@ -17,13 +19,17 @@ public class ABController {
     @Autowired
     private MyService service;
 
+    @GetMapping("/")
+    public String getHomepage() {
+        logMessage("Someone entered the homepage.");
+        return "Welcome to the author & book database!";
+    }
+
     @GetMapping("/con")
     public ArrayList<AuthorBook> getABconns() {
         logMessage("Returning all author/book connections.");
         return service.getAbConns();
     }
-
-
 
     @PutMapping("/con/{authorID}+{bookID}")
     public AuthorBook addConnection(@PathVariable int authorID, @PathVariable int bookID) {
